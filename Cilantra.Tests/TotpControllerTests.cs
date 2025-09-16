157873585b05a4ed211c6b1d5119822af3c3f9fd
Hwai-En Ho <dev@hwaien.com>
2025-09-15 21:34:49 -0700

    adding tests

---
+++ Cilantra.Tests/TotpControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+using Cilantra.Data;
+using Cilantra.Controllers;
+
+namespace Cilantra.Tests;
+
+public class TotpControllerTests
+{
+    private CilantraDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<CilantraDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        return new CilantraDbContext(options);
+    }
+
+    [Fact]
+    public async Task GetAuthenticatorKey_CreatesUser_IfUserIsNew()
+    {
+        // Arrange
+        var newUsername = "NEWUSER";
+        var context = GetInMemoryDbContext();
+        var loggerMock = new Mock<ILogger<TotpController>>();
+        var sut = new TotpController(context, loggerMock.Object);
+        var userExists = await context.Users.AnyAsync(u => u.Username == newUsername);
+        Assert.False(userExists);
+
+        // Act
+        await sut.GetAuthenticatorKey(newUsername, "device");
+
+        // Assert
+        userExists = await context.Users.AnyAsync(u => u.Username == newUsername);
+        Assert.True(userExists);
+    }
+}
